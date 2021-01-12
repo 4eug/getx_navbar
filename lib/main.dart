53558e0abd30c130ec34957e68692e0fe6be16ad
4eug <debraheug@gmail.com>
2021-01-12 05:50:00 +0000

    ....

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'An Example Of GetX_NavBar',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.black,
       ),
