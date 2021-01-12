bf72b609e935b3190ed187682c31a26db6201b55
4eug <debraheug@gmail.com>
2021-01-12 05:45:23 +0000

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'An Example Of GetX_NavBar',
+      theme: ThemeData(
+        primaryColor: Colors.black,
+      ),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  final NavController navController = Get.put(NavController());
+
+  final List<Widget> bodyContent = [
+    Text("Home"),
+    Text("Search"),
+    Text("Messages"),
+    Text("Notifications"),
+    Text("Profile"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "GetXNavBar",
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0),
+          ),
+        ),
+      ),
+      body: Obx(() => Center(
+            child: bodyContent.elementAt(navController.selectedIndex),
+          )),
+      bottomNavigationBar: Obx(
+        () => BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "Search",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: "Messages",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: "Notifications",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+            ),
+          ],
+          currentIndex: navController.selectedIndex,
+          onTap: (index) => navController.selectedIndex = index,
+        ),
+      ),
+    );
+  }
+}
+
+class NavController extends GetxController {
+  final _selectedIndex = 0.obs;
+
+  get selectedIndex => this._selectedIndex.value;
+  set selectedIndex(index) => this._selectedIndex.value = index;
+}
